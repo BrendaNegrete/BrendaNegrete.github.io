223a1dc4f19baae9819d863f6b77820f7cc74b97
BrendaNegrete <151174130+BrendaNegrete@users.noreply.github.com>
2023-12-11 20:18:38 -0600

    Update WindowLoginController.js

--- app/view/login/WindowLoginController.js
+++ app/view/login/WindowLoginController.js
@@ -4,14 +4,16 @@ Ext.define('app.view.login.WindowLoginController',{
     alias:'controller.windowloginctr',
 
     hacerLogin: function (){
-        var formulario = this.lookupReference('formulario');//Obtener referencia al formulario
-        formulario.getForm().submit({ //Enviar la solicitud al servidor
+        var datos = this.lookupReference('formulario').getValues();//Obtener referencia al formulario
+        Ext.Ajax.request({ //Enviar la solicitud al servidor
             url: 'server/dologin.json', //URL para la solicitud de inicio de sesión
             scope: this,
+            params: datos,
             success: function(response,opts){//Función ejecutada en caso de éxito
-                console.dir(opts.result);
+                var obj=Ext.decode(response.responseText);
+                console.dir(obj);
                 this.getView().close();
-                Ext.Msg.alert('Hola', 'Bienvenido de nuevo '+ opts.result.fullname);
+                Ext.Msg.alert('Hola', 'Bienvenido de nuevo '+ obj.fullname);
             },
             failure: function(response, opts){ // Función ejecutada en caso de error
 
